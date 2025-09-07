a752fbaa467ee2891f4bfbac079663dba936903c
josephineohwifo <josephine.ohwifo@kodehauz.com>
2025-09-07 23:53:47 +0100

    My second assignment uploaded

---
+++ Assignmnt-Two.js
@@ -0,0 +1,31 @@
+//Declare three variables: num1, num2, and name. 
+let num1; 
+let num2;
+let name;
+
+//Assign number values to num1 and num2, and a string value to name. 
+num1 = 30; 
+num2 = 3;
+name = 'Josephine';
+
+//Addition (num1 + num2) 
+let sum = num1 + num2
+ console.log(sum)
+
+//Subtraction (num1 - num2) 
+let subtract = num1 - num2
+console.log(subtract)
+
+//Multiplication (num1 * num2) 
+let multiply = num1 * num2
+console.log(multiply)
+
+// Division (num1 / num2) 
+ let division = num1 / num2
+ console.log(division)
+
+ // Print a message: "Hello [name], the result of num1 + num2 is [result]"
+ console.log (`Hello ${name}, the result of num1 + num2 is ${sum}`)
+ 
+
+ 
